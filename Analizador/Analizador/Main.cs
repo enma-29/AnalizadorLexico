c093b142b372d4fe88b6765cf35414de590bbc56
felix <felixenmanuel@hotmail.com>
2022-01-30 18:10:05 -0400

    Analizador lexico. Setup y analzidor.aplicacion para el ejecutable

---
+++ Analizador/Analizador/Main.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Analizador
+{
+    public partial class frmPrincipal : Form
+    {
+        List<char> _numeros = new List<char>(new char[] { '0','1','2','3','4','5','6','7','8','9'});
+        List<char> _variables = new List<char>(new char[] { 'A', 'B', 'C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z'});
+        List<char> _operadores = new List<char>(new char[] { '+', '-', '*', '/' });
+        List<char> _delimitadores = new List<char>(new char[] { '(', ')','{','}' });
+        List<string> _otros= new List<string>(new char[] { '.', "int"});
+        DataTable _tblResultados = new DataTable();
+
+        public frmPrincipal()
+        {
+            InitializeComponent();           
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            _tblResultados.Columns.Add("Token", typeof(string));
+            _tblResultados.Columns.Add("Tipo", typeof(string));
+        }      
+
+        private void btnAnalizar_Click(object sender, EventArgs e)
+        {
+            _tblResultados.Clear();
+
+            List<char> _elementos = txtExpresion.Text.Replace(" ","").ToCharArray().ToList();
+
+            if (_elementos.Count > 0)
+            {
+                DataRow _fila;
+
+                foreach (string elemento in _elementos)
+                {
+                    _fila = _tblResultados.NewRow();
+
+                    if (_numeros.Contains(elemento))
+                    {
+                        _fila["Token"] = elemento;
+                        _fila["Tipo"] = "Número";
+                    }
+                    else if (_variables.Contains(elemento.ToString().ToUpper()[0]))
+                    {
+                        _fila["Token"] = elemento;
+                        _fila["Tipo"] = "Variable";
+                    }
+                    else if (_operadores.Contains(elemento))
+                    {
+                        _fila["Token"] = elemento;
+                        _fila["Tipo"] = "Operador";
+                    }
+                    else if (_delimitadores.Contains(elemento))
+                    {
+                        _fila["Token"] = elemento;
+                        _fila["Tipo"] = "Delimitador";
+                    }
+                     else if (_otros.Contains(elemento))
+                    {
+                        _fila["Token"] = elemento;
+                        _fila["Tipo"] = "otros";
+                    }
+                    else
+                    {
+                        _fila["Token"] = elemento;
+                        _fila["Tipo"] = "Error";
+                    }
+
+                    _tblResultados.Rows.Add(_fila);
+                }
+
+                dgvResultados.DataSource = _tblResultados;
+                dgvResultados.Refresh();
+            }
+            else
+            {
+                dgvResultados.DataSource = null;
+                dgvResultados.Refresh();
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtExpresion.Clear();
+            _tblResultados.Clear();
+            dgvResultados.DataSource = null;
+            dgvResultados.Refresh();
+        }       
+    }
+}
---
+++ Analizador/Analizador/frmPrincipal.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Analizador
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstrucciones = new System.Windows.Forms.Label();
+            this.btnAnalizar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.dgvResultados = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtExpresion = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblInstrucciones
+            // 
+            this.lblInstrucciones.AutoSize = true;
+            this.lblInstrucciones.Location = new System.Drawing.Point(192, 9);
+            this.lblInstrucciones.Name = "lblInstrucciones";
+            this.lblInstrucciones.Size = new System.Drawing.Size(123, 29);
+            this.lblInstrucciones.TabIndex = 0;
+            this.lblInstrucciones.Text = "Expresión:";
+            // 
+            // btnAnalizar
+            // 
+            this.btnAnalizar.Location = new System.Drawing.Point(35, 380);
+            this.btnAnalizar.Name = "btnAnalizar";
+            this.btnAnalizar.Size = new System.Drawing.Size(75, 30);
+            this.btnAnalizar.TabIndex = 3;
+            this.btnAnalizar.Text = "Analizar";
+            this.btnAnalizar.UseVisualStyleBackColor = true;
+            this.btnAnalizar.Click += new System.EventHandler(this.btnAnalizar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(181, 380);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 30);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // dgvResultados
+            // 
+            this.dgvResultados.ColumnHeadersHeight = 34;
+            this.dgvResultados.Location = new System.Drawing.Point(35, 129);
+            this.dgvResultados.Name = "dgvResultados";
+            this.dgvResultados.RowHeadersWidth = 62;
+            this.dgvResultados.Size = new System.Drawing.Size(468, 225);
+            this.dgvResultados.TabIndex = 5;
+            this.dgvResultados.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvResultados_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(340, 376);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 38);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Salir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtExpresion
+            // 
+            this.txtExpresion.Location = new System.Drawing.Point(49, 60);
+            this.txtExpresion.Name = "txtExpresion";
+            this.txtExpresion.Size = new System.Drawing.Size(454, 36);
+            this.txtExpresion.TabIndex = 8;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(540, 494);
+            this.Controls.Add(this.txtExpresion);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvResultados);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnAnalizar);
+            this.Controls.Add(this.lblInstrucciones);
+            this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Analizador";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstrucciones;
+        private System.Windows.Forms.Button btnAnalizar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.DataGridView dgvResultados;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtExpresion;
+    }
+}
+
